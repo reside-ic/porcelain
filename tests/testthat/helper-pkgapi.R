1840db00b53ee7673d1f5ec9441311dd2cd0ceea
Rich FitzJohn <rich.fitzjohn@gmail.com>
2020-02-21 08:38:43 +0000

    Add helper

---
+++ tests/testthat/helper-pkgapi.R
@@ -0,0 +1,17 @@
+## TODO: required with current plumber?
+options(plumber.debug = FALSE)
+
+## TODO: this moves elsewhere
+test_call <- function(p, verb, path) {
+  req <- new.env(parent = emptyenv())
+  req$REQUEST_METHOD <- toupper(verb)
+  req$PATH_INFO <- path
+  req$QUERY_STRING <- ""
+  req$rook.input <- list(read_lines = function() "")
+
+  ## TODO: we might not really need the whole thing here, but this is
+  ## a problem potentially.  The format is described and conforms to
+  ## the Rook interface but this class is not exported.
+  res <- plumber:::PlumberResponse$new()
+  p$serve(req, res)
+}
