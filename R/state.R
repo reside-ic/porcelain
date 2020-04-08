55391c722e8293bc705ae8f312f801c7eb53e334
Rich FitzJohn <rich.fitzjohn@gmail.com>
2020-04-08 09:30:54 +0100

    Depend on 3.5 due to rds

--- R/state.R
+++ R/state.R
@@ -1,3 +1,26 @@
+##' Bind state into an endpoint
+##'
+##' This method allows state to be bound to the target function.  Each
+##' element of \code{...} (or \code{.state}) is named with the
+##' argument to the target function being bound, and the value is the
+##' value that argument will take.  Once bound, the arguments to the
+##' target function may not be provided by an input.
+##'
+##' The primary use case for this is to bind mutable state (database
+##' connections, etc) that may be shared amongst different endpoints
+##' within an API.
+##'
+##' @title Bind state into an endpoint
+##'
+##' @param ... Named arguments representing state to bind; see
+##'   \code{Details}.
+##'
+##' @param .state A list of named state to bind, instead
+##'   of using \code{...} - this interface is considerably easier to
+##'   program against if building an API programmatically, avoiding
+##'   the use of \code{\link{do.call}}.
+##'
+##' @export
 pkgapi_state <- function(..., .state = list(...)) {
   assert_named(.state, TRUE)
   pkgapi_state_collection$new(.state)
