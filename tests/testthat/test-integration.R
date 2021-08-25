2a30bbc00a5e7453290f1b1740d742368e5aceff
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2021-08-25 17:58:22 +0100

    Revert "Try alternative approach to test"
    This reverts commit 9fee5ff1a17baffacf761933ed77eec3415e0b7e.

--- tests/testthat/test-integration.R
+++ tests/testthat/test-integration.R
@@ -2,13 +2,16 @@ context("integration")
 
 test_that("Can run add package", {
   skip_on_cran()
-  skip_if_not_installed("pkgload")
-
+  lib <- tempfile()
+  dir.create(lib, FALSE, TRUE)
+  on.exit(unlink(lib, recursive = TRUE))
+  withr::local_libpaths(lib, "prefix")
   add <- system_file("examples/add", package = "porcelain")
-  pkg <- pkgload::load_all(add, export_all = FALSE)
-  on.exit(pkgload::unload("add"))
 
-  api <- pkg$env$api(TRUE)
+  system2("R", c("CMD", "INSTALL", paste0("--library=", lib), add),
+          stdout = FALSE, stderr = FALSE)
+  env <- loadNamespace("add", lib)
+  api <- env$api(TRUE)
   res <- api$request("GET", "/", c(a = 1, b = 2))
   expect_equal(res$headers[["X-Porcelain-Validated"]], "true")
   expect_equal(from_json(res$body)$status, "success")
