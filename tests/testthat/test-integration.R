cfe30cd79630433e6d3cb276bded7e35d90acbad
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2021-08-26 10:09:04 +0100

    Back to pkgload over remotes for test now

--- tests/testthat/test-integration.R
+++ tests/testthat/test-integration.R
@@ -2,16 +2,12 @@ context("integration")
 
 test_that("Can run add package", {
   skip_on_cran()
-  skip_if_not_installed("remotes")
-  skip_on_os("windows") # usual issues getting packages installed on CI
-  lib <- tempfile()
-  dir.create(lib, FALSE, TRUE)
-  on.exit(unlink(lib, recursive = TRUE))
-  withr::local_libpaths(lib, "prefix")
-  add <- system_file("examples/add", package = "porcelain")
-  remotes::install_local(add, lib = lib, force = TRUE)
-  env <- loadNamespace("add", lib)
-  api <- env$api(TRUE)
+  skip_if_not_installed("pkgload")
+  path_pkg <- system_file("examples/add", package = "porcelain")
+  pkg <- pkgload::load_all(path_pkg, export_all = FALSE)
+  on.exit(pkgload::unload("add"))
+
+  api <- pkg$env$api(TRUE)
   res <- api$request("GET", "/", c(a = 1, b = 2))
   expect_equal(res$headers[["X-Porcelain-Validated"]], "true")
   expect_equal(from_json(res$body)$status, "success")
